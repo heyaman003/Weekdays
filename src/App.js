c78580ee626c35b6812d2ade633414c1e5afcf9c
Syed Fahad Junaid <syedfahadjunaid@gmail.com>
2024-05-01 11:32:16 +0530

    impplmented api request

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useGetJobsQuery } from "./Store/APIServices/JobData";
 function App() {
   const responseGetJobs = useGetJobsQuery();
 
-  console.log(responseGetJobs);
+  // console.log(responseGetJobs);
 
   return <div className='App'>Hello</div>;
 }
